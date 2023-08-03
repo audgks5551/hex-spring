60a8ab721232e18bd0f57401b9a32d9788d74be9
audgks5551 <audgsk5551@daum.net>
2023-08-03 18:17:05 +0900

    주문 로직 구현

---
+++ order-service/order-domain/order-application-service/src/main/java/org/example/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,75 @@
+package org.example.order.service.domain.mapper;
+
+import org.example.domain.valueobject.CustomerId;
+import org.example.domain.valueobject.Money;
+import org.example.domain.valueobject.ProductId;
+import org.example.domain.valueobject.RestaurantId;
+import org.example.order.service.domain.dto.create.CreateOrderCommand;
+import org.example.order.service.domain.dto.create.CreateOrderResponse;
+import org.example.order.service.domain.dto.create.OrderAddress;
+import org.example.order.service.domain.entity.Order;
+import org.example.order.service.domain.entity.OrderItem;
+import org.example.order.service.domain.entity.Product;
+import org.example.order.service.domain.entity.Restaurant;
+import org.example.order.service.domain.valueobject.StreetAddress;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderDataMapper {
+
+    public Restaurant createOrderCommandToRestaurant(CreateOrderCommand createOrderCommand) {
+
+        List<Product> products = createOrderCommand.getItems().stream()
+                .map(orderItem -> new Product(new ProductId(orderItem.getProduceId())))
+                .collect(Collectors.toList());
+
+        RestaurantId restaurantId = new RestaurantId(createOrderCommand.getRestaurantId());
+
+        return Restaurant.builder()
+                .restaurantId(restaurantId)
+                .products(products)
+                .build();
+    }
+
+    public Order createOrderCommandToOrder(CreateOrderCommand createOrderCommand) {
+        return Order.builder()
+                .customerId(new CustomerId(createOrderCommand.getCustomerId()))
+                .restaurantId(new RestaurantId(createOrderCommand.getRestaurantId()))
+                .deliveryAddress(orderAddressToStreetAddress(createOrderCommand.getAddress()))
+                .price(new Money(createOrderCommand.getPrice()))
+                .items(orderItemsToOrderItemEntities(createOrderCommand.getItems()))
+                .build();
+    }
+
+    public CreateOrderResponse orderToCreateOrderResponse(Order order) {
+        return CreateOrderResponse.builder()
+                .orderTrackingId(order.getTrackingId().getValue())
+                .orderStatus(order.getOrderStatus())
+                .build();
+    }
+
+    private List<OrderItem> orderItemsToOrderItemEntities(List<org.example.order.service.domain.dto.create.OrderItem> orderItems) {
+        return orderItems.stream()
+                .map(orderItem ->
+                        OrderItem.builder()
+                                .product(new Product(new ProductId(orderItem.getProduceId())))
+                                .price(new Money(orderItem.getPrice()))
+                                .quantity(orderItem.getQuantity())
+                                .subTotal(new Money(orderItem.getSubTotal()))
+                                .build()
+                ).collect(Collectors.toList());
+    }
+
+    private StreetAddress orderAddressToStreetAddress(OrderAddress orderAddress) {
+        return new StreetAddress(
+                UUID.randomUUID(),
+                orderAddress.getStreet(),
+                orderAddress.getPostalCode(),
+                orderAddress.getCity()
+        );
+    }
+}
