f724af3a0ad5077ffa6386c51bda35a53587fa44
audgks5551 <audgsk5551@daum.net>
2023-08-03 18:29:13 +0900

    Implemented event publishing

--- order-service/order-domain/order-application-service/src/main/java/org/example/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/org/example/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
@@ -4,6 +4,4 @@ import org.example.domain.event.publisher.DomainEventPublisher;
 import org.example.order.service.domain.event.OrderCreatedEvent;
 
 public interface OrderCreatedPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCreatedEvent> {
-
-
 }
